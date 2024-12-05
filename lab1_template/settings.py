1dafaed7eca9a65d710d28e1004657d56c9755aa
dchen376 <dongping07@gmail.com>
2024-12-04 21:41:42 -0500

    first Django ORM app with first Django model

---
+++ lab1_template/settings.py
@@ -0,0 +1,17 @@
+# Postgre SQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'ODwhkHsDsw1H0jtW4i2QF0W7',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'orm',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
